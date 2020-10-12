c6e7a5ca268578ace78fb63671ad1abd81c338c6
github username <github email address>
2020-10-12 14:15:22 -0600

    added show and destroy on cat controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,8 +1,24 @@
 class CatsController < ApplicationController
+    before_action :find_cat, only: [:destroy, :show]
     def index
         @cats = Cat.all
-
         render json: @cats
     end
 
+    def show 
+        render json: @cat
+    end
+
+    def destroy
+        @cat.destroy
+
+        render json: {message: "#{@cat.name} was destroyed"}
+    end
+
+    private 
+
+    def find_cat 
+        @cat = Cat.find(params[:id])
+    end
 end
+
