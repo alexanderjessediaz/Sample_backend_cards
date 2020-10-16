57ea86c6b3bdfe4c2556116deea0fe52b4e96962
github username <github email address>
2020-10-16 14:55:20 -0600

    Backend tweaks and updates

--- app/models/cat_card.rb
+++
@@ -1,2 +0,0 @@
-class CatCard < ApplicationRecord
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@ Cat.create(name:"Cleo", age:3, weight:7.2,image:"https://upload.wikimedia.org/wi
 Cat.create(name:"Jasper", age:1, weight:5.6,image:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/A_black_cat_named_Tilly.jpg/220px-A_black_cat_named_Tilly.jpg")
 Cat.create(name:"Pinky", age:8, weight:9.3,image:"https://cdn.pixabay.com/photo/2016/08/04/21/20/cat-1570991_960_720.jpg")
 Cat.create(name:"Moose", age:5, weight:8.0,image:"https://indoorpet.osu.edu/sites/indoorpet/files/assets/images/cats/Calico.jpg")
-
+Cat.create(name:"Mouse", age:2, weight:10.0,image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_CXYuWiCTM8KT21V_PijuwzzcUPwAvFaxSw&usqp=CAU")
